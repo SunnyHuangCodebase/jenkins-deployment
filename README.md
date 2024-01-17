69ced4ea5d252038be83c4001a157776d44fe6a8
Sunny Huang <106368104+SunnyHuangCodebase@users.noreply.github.com>
2024-01-17 13:02:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Deploy Jenkins on Minikube
+Setup Jenkins on a Minikube Kubernetes cluster using Pulumi's Infrastructure as Code written in Go or  `kubectl` commands.  
+
+## Prerequisites
+- [Docker Desktop](https://www.docker.com/products/docker-desktop/)
+- [Minikube](https://minikube.sigs.k8s.io/docs/start)
+- [Pulumi](https://www.pulumi.com/docs/install/) or [kubectl](https://kubernetes.io/docs/tasks/tools/)
+
+### Change To Working Directory
+Pulumi:
+```sh
+cd /path/to/this/project/jenkins-deployment
+```
+
+Kubectl:
+```sh
+cd /path/to/this/project/jenkins-deployment/manifests
+``` 
+
+### Create/Update Resources
+Pulumi
+```sh
+pulumi up --yes --skip-preview
+```
+
+Kubectl:
+```sh
+kubectl apply -f jenkins.yaml
+```
+
+### Forward Jenkins Service Port
+```sh
+kubectl port-forward `kubectl get service -o name | grep jenkins` 8000:8000
+```
+
+### Access Jenkins:
+[http://localhost:8000](http://localhost:8000)
+
+### Delete Resources
+Pulumi:
+```sh
+pulumi destroy --skip-preview --yes
+```
+
+Kubectl:
+```sh
+kubectl delete -f jenkins.yaml
+```
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	apps "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/apps/v1"
+	core "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
+	meta "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		appName := "jenkins"
+		appLabels := pulumi.StringMap{
+			"app": pulumi.String(appName),
+		}
+
+		deployment, err := apps.NewDeployment(ctx, "jenkins-deployment", &apps.DeploymentArgs{
+			Spec: apps.DeploymentSpecArgs{
+				Selector: &meta.LabelSelectorArgs{
+					MatchLabels: appLabels,
+				},
+				Replicas: pulumi.Int(1),
+				Template: &core.PodTemplateSpecArgs{
+					Metadata: &meta.ObjectMetaArgs{
+						Labels: appLabels,
+					},
+					Spec: &core.PodSpecArgs{
+						Containers: core.ContainerArray{
+							core.ContainerArgs{
+								Name:  pulumi.String(appName),
+								Image: pulumi.String("jenkins/jenkins"),
+								Ports: core.ContainerPortArray{
+									core.ContainerPortArgs{
+										ContainerPort: pulumi.Int(8080),
+										Name:          pulumi.String("http"),
+									},
+								},
+							},
+						},
+						Volumes: core.VolumeArray{
+							core.VolumeArgs{
+								Name: pulumi.String("jenkins-data"),
+								EmptyDir: &core.EmptyDirVolumeSourceArgs{
+									Medium: pulumi.String("Memory"),
+								},
+							},
+						},
+					},
+				},
+			},
+		})
+
+		if err != nil {
+			return err
+		}
+
+		frontend, err := core.NewService(ctx, appName, &core.ServiceArgs{
+			Metadata: &meta.ObjectMetaArgs{
+				Labels: appLabels,
+			},
+			Spec: &core.ServiceSpecArgs{
+				Type: pulumi.String("ClusterIP"),
+				Ports: &core.ServicePortArray{
+					core.ServicePortArgs{
+						Port:       pulumi.Int(8000),
+						TargetPort: pulumi.Int(8080),
+						Protocol:   pulumi.String("TCP"),
+					},
+				},
+				Selector: appLabels,
+			},
+		})
+
+		ctx.Export("name", deployment.Metadata.Name())
+		ctx.Export("frontendIp", frontend.Spec.ClusterIP())
+		//TODO: Add authorization key export
+
+		return nil
+	})
+}
